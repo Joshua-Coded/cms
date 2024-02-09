83df8b803efc07e206e2def9537171c8089042b5
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-09 08:55:46 +0100

    userrepository included

---
+++ src/main/java/com/alana/cms/domain/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.alana.cms.domain.repository;
+
+import com.alana.cms.domain.models.Category;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Joshua Alana 09/02/24
+ */
+@Service
+public class CategoryRepository extends AbstractRepository<Category> {
+}
