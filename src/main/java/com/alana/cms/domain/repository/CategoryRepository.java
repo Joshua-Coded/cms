9ebe885ef4a8667640e0c2fabcfd67b53fdde568
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 17:58:32 +0100

    reposotiry

---
+++ src/main/java/com/alana/cms/domain/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.alana.cms.domain.repository;
+
+import com.alana.cms.domain.models.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryRepository extends AbstractRepository<Category> {
+}
