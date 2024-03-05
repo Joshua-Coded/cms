43854355136d768cb65a532c4ef3d85ab52a692f
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 16:25:35 +0100

    tag and category class rewritten

---
+++ src/main/java/com/alana/cms/domain/models/Category.java
@@ -0,0 +1,12 @@
+package com.alana.cms.domain.models;
+
+import lombok.Data;
+
+/*
+@author Joshua Alana
+ */
+@Data
+public class Category {
+    String name;
+    String id;
+}
---
+++ src/main/java/com/alana/cms/domain/models/Tag.java
@@ -0,0 +1,11 @@
+package com.alana.cms.domain.models;
+
+import lombok.Data;
+
+@Data
+public class Tag {
+    /*
+    @author Joshua Alana
+     */
+        String value;
+}
