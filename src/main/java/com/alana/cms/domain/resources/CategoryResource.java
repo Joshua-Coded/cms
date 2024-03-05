eb35aad0fa4cce6566e757239b848f91756c0e2b
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 22:46:16 +0100

    swagger annotations

--- src/main/java/com/alana/cms/domain/resources/CategoryResource.java
+++ src/main/java/com/alana/cms/domain/resources/CategoryResource.java
@@ -3,6 +3,10 @@ package com.alana.cms.domain.resources;
 
 import com.alana.cms.domain.models.Category;
 import com.alana.cms.domain.vo.CategoryRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,30 +16,57 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@Api(tags = "category", description = "Category API")
 public class CategoryResource {
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(value="Find category", notes = "Find the category by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category found"),
+            @ApiResponse(code = 404, message = "Category not found")
+    })
     public ResponseEntity<Category> findOne(@PathVariable("id") String id){
         return ResponseEntity.ok(new Category());
     }
 
     @GetMapping
+    @ApiOperation(value="List categories", notes = "list all categories")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Categories found"),
+            @ApiResponse(code = 404, message = "Categories not found")
+    })
     public ResponseEntity<List<Category>> findAll(){
         return ResponseEntity.ok(Arrays.asList(new Category(), new Category()));
     }
 
     @PostMapping
+    @ApiOperation(value="create category", notes = "it permits to create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Category created successfully"),
+            @ApiResponse(code = 400, message = "invalid request")
+    })
     public ResponseEntity<Category> newCategory(CategoryRequest category){
         return new ResponseEntity<>(new Category(), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value="remove category", notes = "it permits to remove category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category removed successfully"),
+            @ApiResponse(code = 404, message = "Category not found")
+    })
     public void removeCategory(@PathVariable("id") String id) {
 
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value="update category", notes = "It permits to update a category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Category updated successfully"),
+            @ApiResponse(code = 404, message = "Category not found"),
+            @ApiResponse(code = 400, message = "invalid request")
+    })
     public ResponseEntity<Category> updateCategory(@PathVariable("id") String id, CategoryRequest category){
         return new ResponseEntity<>(new Category(), HttpStatus.OK);
     }
