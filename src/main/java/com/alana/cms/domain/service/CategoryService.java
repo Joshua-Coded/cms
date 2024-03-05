cf1ff9784b1a8c3269cdd2b0ef87fd56f707c9ba
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 18:08:13 +0100

    service

---
+++ src/main/java/com/alana/cms/domain/service/CategoryService.java
@@ -0,0 +1,40 @@
+package com.alana.cms.domain.service;
+
+import com.alana.cms.domain.models.Category;
+import com.alana.cms.domain.repository.CategoryRepository;
+import com.alana.cms.domain.vo.CategoryRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category update(Category category) {
+        return this.categoryRepository.save(category);
+    }
+
+    public Category create(CategoryRequest request){
+        Category category = new Category();
+        category.setName(request.getName());
+        return this.categoryRepository.save(category);
+    }
+
+    public void delete(String id){
+        final Category category = this.categoryRepository.findOne(id);
+        this.categoryRepository.delete(category);
+    }
+
+    public List<Category> findAll(){
+        return this.categoryRepository.findAll();
+    }
+
+    public Category findOne(String id){
+        return this.categoryRepository.findOne(id);
+    }
+}
