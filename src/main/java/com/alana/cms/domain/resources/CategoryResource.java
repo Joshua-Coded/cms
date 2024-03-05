d6cd2e8abc7234b257de93040cf84357819b47ae
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 16:58:40 +0100

    category resources

---
+++ src/main/java/com/alana/cms/domain/resources/CategoryResource.java
@@ -0,0 +1,42 @@
+package com.alana.cms.domain.resources;
+
+
+import com.alana.cms.domain.models.Category;
+import com.alana.cms.domain.vo.CategoryRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findOne(@PathVariable("id") String id){
+        return ResponseEntity.ok(new Category());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        return ResponseEntity.ok(Arrays.asList(new Category(), new Category()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> newCategory(CategoryRequest category){
+        return new ResponseEntity<>(new Category(), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCategory(@PathVariable("id"), String id) {
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") String id, CategoryRequest category){
+        return new ResponseEntity<>(new Category(), HttpStatus.OK);
+    }
+}
